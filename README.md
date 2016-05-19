4fd9779c12509394e81db21705c70da3e6e271d5
carlos perez <charlsagente@gmail.com>
2016-05-19 16:58:22 -0500

    geolocalization
    Getting geolocalization and displaying its weather using 3rd party API.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ##Please visit [http://charlsagente.github.io/](http://charlsagente.github.io/)
+and see this code running
--- local-weather/js/operations.js
+++ local-weather/js/operations.js
@@ -3,14 +3,33 @@
  */
 
 $(document).ready(function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            //$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+            $.ajax({
+                method: "GET",
+                url: "http://api.openweathermap.org/data/2.5/weather",
+                data: { APPID: "8d71992a9fc0411860572517d49978ff", lat: position.coords.latitude,lon: position.coords.longitude }
+            })
+                .done(function( msg ) {
+                    console.log( msg.weather[0].id );
+                    $.getJSON( "js/mapping.json", function( data ) {
 
-    $.ajax({
-    method: "GET",
-    url: "api.openweathermap.org/data/2.5/weather",
-    data: { APPID: "8d71992a9fc0411860572517d49978ff", lat: "35",lon:"139" }
-    })
-    .done(function( msg ) {
-        alert( "Data Saved: " + msg );
-    });
+                        if(data.hasOwnProperty(msg.weather[0].id)){
+                            $('#weather-icon').removeClass();
+                            $('#weather-icon').addClass('wi customize-icons '+data[msg.weather[0].id]);
+
+                        }
+                        else{
+                            $('#weather-icon').removeClass();
+                            $('#weather-icon').addClass('wi customize-icons wi-day-sunny');
+                        }
+                    });
+                    $('div span').text(msg.name+", "+msg.sys.country);
+
+                });
+        });
+    }
+
 
 });
