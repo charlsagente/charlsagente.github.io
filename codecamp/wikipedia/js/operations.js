eb0e5ec6001f432325506bb2a842efa0d78be3d7
carlos perez <charlsagente@gmail.com>
2016-05-27 15:04:35 -0500

    wikipedia viewer :notes:
    Added wikipedia viewer

--- codecamp/wikipedia/js/operations.js
+++ codecamp/wikipedia/js/operations.js
@@ -3,41 +3,71 @@
  */
 $(document).ready(function(){
 
-$("#searchclear").click(function(){
-    $("#searchinput").val('');
-});
+    $("#searchclear").click(function(){
+        $("#searchinput").val('');
+        $("#searchinput").focus();
+        $( ".list-group" ).empty();
+    });
+
+    $("#searchinput").keyup(function (e) {
+        if (e.keyCode == 13) {
+
+            $.ajax({
+
+                method:"GET",
+                dataType: 'jsonp',
+                jsonp: "callback",
+                headers: {
+
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Api-User-Agent': 'Example/1.0'
+
+                },
+                url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&callback=JSON_CALLBACK&gsrsearch="+encodeURI($("#searchinput").val()),
+
+                success: function (msg) {
+                    console.log(msg.query.pages);
+
+                    $( ".list-group" ).empty();
+
+                    $.each(msg.query.pages,function(index, jsonelement){
+
+                        $( "<a>" ,
+                            {href:'https://en.wikipedia.org/?curid='+jsonelement.pageid,
+                                target:'_blank',
+                                class:'list-group-item'
+                            }).append(
+
+                            $("<h4>",{
+                                text:jsonelement.title,
+                                class:'list-group-item-heading'
+
+                            }),
+                            $("<p>",{
+                                text:jsonelement.extract,
+                                class:'list-group-item-text'
+                            })
+                        ).appendTo('.list-group');
+
+                        $('<br>').appendTo('.list-group');
 
 
-        $.ajax({
 
-            method:"POST",
-            dataType: 'jsonp',
-            jsonp: "callback",
-            headers: {
-
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Api-User-Agent': 'Example/1.0'
-
-            },
-            url: "https://en.wikipedia.org/w/api.php?action=query&titles=Racism&prop=revisions&rvprop=content&format=jsonfm",
-            data: {
-                action: "query",
-                list: "search",
-                srsearch: "javascript",
-                format: "json"
-            },
+                    });
 
 
-            success: function (msg) {
-                console.log(msg);
+
+                },
+                error: function (xhr, opts, error) {
+                    alert("An error has ocurred, maybe https because security");
+                    console.log(error);
+                }
+            });
+        }
+    });
 
 
-            },
-            error: function (xhr, opts, error) {
-                alert("An error has ocurred, maybe https because security");
-                console.log(error);
-            }
-        })
+
 
 });
